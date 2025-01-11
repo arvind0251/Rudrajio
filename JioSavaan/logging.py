75bff077d54ecc1372e4483f2f60d568d6276857
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-01-11 10:32:48 +0530

    Update logging.py

--- JioSavaan/logging.py
+++ JioSavaan/logging.py
@@ -1,20 +1,21 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    level=logging.DEBUG,  # Changed log level to DEBUG for detailed logging.
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(module)s:%(lineno)d - %(message)s",  # Added module and line number.
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        logging.FileHandler("log.txt"),
+        RotatingFileHandler("log.txt", maxBytes=1000000, backupCount=3),  # Use rotating file handler.
         logging.StreamHandler(),
     ],
 )
 
+# Set log level to ERROR for specific libraries to reduce noise in logs.
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
